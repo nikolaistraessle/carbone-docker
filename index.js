363cfede40fec84fef6107074f832109234c11aa
Nikolai Strässle <nst@arcade.ch>
2020-12-20 16:02:41 +0100

    add document as base64 json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require(`path`);
-const fs = require(`fs-extra`);
+const fs = require(`fs`);
 const _ = require(`lodash`);
 const util = require(`util`);
 const carbone = require(`carbone`);
@@ -7,11 +7,11 @@ const telejson = require(`telejson`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const app = express();
-const upload = require(`multer`)({ dest: `/tmp-reports/` });
 const port = process.env.CARBONE_PORT || 3030;
+const workdir = '/tmp/'
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const render = util.promisify(carbone.render);
 
@@ -19,54 +19,59 @@ const render = util.promisify(carbone.render);
 _.forEach(carbone.formatters, formatter => formatter.$isDefault = true);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(`./test.html`));
+    res.sendFile(path.resolve(`./test.html`));
 });
 
-app.post('/render', upload.single(`template`), async (req, res) => {
-  const template = req.file;
-  const originalNameWOExt = template.originalname.split(`.`).slice(0, -1).join(`.`);
-  const originalFormat = template.originalname.split(`.`).reverse()[0];
-  let data = req.body.data;
-  let options = {};
-  let formatters = {};
-  try {
-    options = JSON.parse(req.body.options);
-  } catch (e) {}
-  options.convertTo = options.convertTo || originalFormat;
-  options.outputName = options.outputName || `${originalNameWOExt}.${options.convertTo}`;
-  if (typeof data !== `object` || data === null) {
+app.post('/render', async (req, res) => {
+    let body = req.body;
+    let fileContent = body.payload;
+    let fileName = body.filename;
+    let data = body.data;
+    fs.writeFileSync(workdir + fileName, fileContent, 'base64');
+    const originalNameWOExt = fileName.split(`.`).slice(0, -1).join(`.`);
+    const originalFormat = fileName.split(`.`).reverse()[0];
+    let options = {};
+    let formatters = {};
     try {
-      data = JSON.parse(req.body.data);
+        options = JSON.parse(req.body.options);
     } catch (e) {
-      data = {};
     }
-  }
-  try {
-    formatters = telejson.parse(req.body.formatters);
-  } catch (e) {}
+    options.convertTo = options.convertTo || originalFormat;
+    options.outputName = options.outputName || `${originalNameWOExt}.${options.convertTo}`;
+    if (typeof data !== `object` || data === null) {
+        try {
+            data = JSON.parse(req.body.data);
+        } catch (e) {
+            data = {};
+        }
+    }
+    try {
+        formatters = telejson.parse(req.body.formatters);
+    } catch (e) {
+    }
 
-  // Removing previous custom formatters before adding new ones
-  carbone.formatters = _.filter(carbone.formatters, formatter => formatter.$isDefault === true);
+    // Removing previous custom formatters before adding new ones
+    carbone.formatters = _.filter(carbone.formatters, formatter => formatter.$isDefault === true);
 
-  carbone.addFormatters(formatters);
+    carbone.addFormatters(formatters);
 
-  let report = null;
+    let report = null;
 
-  try {
-    report = await render(template.path, data, options);
-  } catch (e) {
-    console.log(e);
-    return res.status(500).send(`Internal server error`);
-  }
+    try {
+        report = await render(workdir + fileName, data, options);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send(`Internal server error`);
+    }
 
-  fs.remove(template.path);
+    fs.unlinkSync(workdir + fileName);
 
-  res.setHeader(`Content-Disposition`, `attachment; filename=${options.outputName}`);
-  res.setHeader(`Content-Transfer-Encoding`, `binary`);
-	res.setHeader(`Content-Type`, `application/octet-stream`);
-  res.setHeader(`Carbone-Report-Name`, options.outputName);
+    res.setHeader(`Content-Disposition`, `attachment; filename=${options.outputName}`);
+    res.setHeader(`Content-Transfer-Encoding`, `binary`);
+    res.setHeader(`Content-Type`, `application/octet-stream`);
+    res.setHeader(`Carbone-Report-Name`, options.outputName);
 
-  return res.send(report);
+    return res.send(report);
 });
 
 app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
