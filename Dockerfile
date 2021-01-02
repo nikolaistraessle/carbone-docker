33f234443b7ba403ecf171555d7187931f17a562
Nikolai Strässle <nst@arcade.ch>
2021-01-02 14:58:06 +0100

    add eslint and use docker slim

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM node:latest
+FROM node:slim
 
 WORKDIR /tmp
-RUN wget http://downloadarchive.documentfoundation.org/libreoffice/old/7.0.4.2/deb/x86_64/LibreOffice_7.0.4.2_Linux_x86-64_deb.tar.gz -O libo.tar.gz
-RUN apt update \
-  && apt install -y libxinerama1 libfontconfig1 libdbus-glib-1-2 libcairo2 libcups2 libglu1-mesa libsm6 unzip \
+RUN apt-get update \
+  && apt-get install -y libxinerama1 libfontconfig1 libdbus-glib-1-2 libcairo2 libcups2 libglu1-mesa libsm6 unzip wget \
+  && wget http://downloadarchive.documentfoundation.org/libreoffice/old/7.0.4.2/deb/x86_64/LibreOffice_7.0.4.2_Linux_x86-64_deb.tar.gz -O libo.tar.gz \
   && tar -zxvf libo.tar.gz
 WORKDIR LibreOffice_7.0.4.2_Linux_x86-64_deb/DEBS
 RUN dpkg -i *.deb
--- index.js
+++ index.js
@@ -1,81 +1,89 @@
-const path = require(`path`);
-const fs = require(`fs`);
-const _ = require(`lodash`);
-const util = require(`util`);
-const carbone = require(`carbone`);
-const telejson = require(`telejson`);
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
-const app = express();
-const port = process.env.CARBONE_PORT || 3030;
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+const carbone = require('carbone')
+const telejson = require('telejson')
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const app = express()
+const port = process.env.CARBONE_PORT || 3030
 const workdir = '/tmp/'
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const render = util.promisify(carbone.render);
+const render = util.promisify(carbone.render)
 
 // Flagging default formatters to remove custom ones later
-_.forEach(carbone.formatters, formatter => formatter.$isDefault = true);
+for (const [key] of Object.entries(carbone.formatters)) {
+  carbone.formatters[key].$isDefault = true
+}
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(`./test.html`));
-});
+  res.sendFile(path.resolve('./test.html'))
+})
 
 app.post('/render', async (req, res) => {
-    let body = req.body;
-    let fileContent = body.payload;
-    let fileName = body.filename;
-    let data = body.data;
-    fs.writeFileSync(workdir + fileName, fileContent, 'base64');
-    const originalNameWOExt = fileName.split(`.`).slice(0, -1).join(`.`);
-    const originalFormat = fileName.split(`.`).reverse()[0];
-    let options = body.options;
-    let formatters = {};
-    options.convertTo = options.convertTo || originalFormat;
-    options.outputName = options.outputName || `${originalNameWOExt}.${options.convertTo}`;
-    if (typeof data !== `object` || data === null) {
-        try {
-            data = JSON.parse(req.body.data);
-        } catch (e) {
-            data = {};
-        }
-    }
+  if (!checkApiToken(req)) {
+    return res.status(401).send('invalid api token')
+  }
+  const body = req.body
+  const fileContent = body.payload
+  const fileName = body.filename
+  let data = body.data
+  fs.writeFileSync(workdir + fileName, fileContent, 'base64')
+  const originalNameWOExt = fileName.split('.').slice(0, -1).join('.')
+  const originalFormat = fileName.split('.').reverse()[0]
+  const options = body.options
+  let formatters = {}
+  options.convertTo = options.convertTo || originalFormat
+  options.outputName = options.outputName || `${originalNameWOExt}.${options.convertTo}`
+  if (typeof data !== 'object' || data === null) {
     try {
-        formatters = telejson.parse(req.body.formatters);
+      data = JSON.parse(req.body.data)
     } catch (e) {
+      data = {}
     }
-
-    // Removing previous custom formatters before adding new ones
-    carbone.formatters = _.filter(carbone.formatters, formatter => formatter.$isDefault === true);
-
-    let tmpFormatters = [];
-
-    for (let k = 0; k < carbone.formatters.length; k++) {
-        if (carbone.formatters[k].hasOwnProperty('name')) {
-            tmpFormatters[carbone.formatters[k].name] = carbone.formatters[k]
-        }
-    }
-    carbone.formatters = tmpFormatters;
-    carbone.addFormatters(formatters);
-
-    let report = null;
-
+  }
+  if (req.body.formatters !== null && undefined !== req.body.formatters) {
     try {
-        report = await render(workdir + fileName, data, options);
+      formatters = telejson.parse(req.body.formatters)
     } catch (e) {
-        console.log(e);
-        return res.status(500).send(`Internal server error`);
+      console.log(e)
     }
+  }
 
-    fs.unlinkSync(workdir + fileName);
+  // Removing previous custom formatters before adding new ones
+  for (const [key] of Object.entries(carbone.formatters)) {
+    if (!carbone.formatters[key].$isDefault) {
+      delete carbone.formatters[key]
+    }
+  }
+  carbone.addFormatters(formatters)
 
-    res.setHeader(`Content-Disposition`, `attachment; filename=${options.outputName}`);
-    res.setHeader(`Content-Transfer-Encoding`, `binary`);
-    res.setHeader(`Content-Type`, `application/octet-stream`);
-    res.setHeader(`Carbone-Report-Name`, options.outputName);
+  let report = null
 
-    return res.send(report);
-});
+  try {
+    report = await render(workdir + fileName, data, options)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send('Internal server error')
+  }
 
-app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
+  fs.unlinkSync(workdir + fileName)
+
+  res.setHeader('Content-Disposition', `attachment; filename=${options.outputName}`)
+  res.setHeader('Content-Transfer-Encoding', 'binary')
+  res.setHeader('Content-Type', 'application/octet-stream')
+  res.setHeader('Carbone-Report-Name', options.outputName)
+
+  return res.send(report)
+})
+
+app.listen(port, () => console.log(`Carbone wrapper listening on port ${port}!`))
+
+function checkApiToken (req) {
+  const token = req.header('X-Api-Token')
+  return process.env.API_TOKEN === null || undefined === process.env.API_TOKEN || token === process.env.API_TOKEN
+}
